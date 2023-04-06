b29440a84f6632a84c44cddde465a54c88b76c37
CjDupre <dupre.pdx@gmail.com>
2023-04-06 15:36:52 -0700

    adding 2nd data set

--- script.js
+++ script.js
@@ -22,6 +22,30 @@ const sites = [
     }
 ];
 
+const sites2 = [
+    {
+        thumbnail: "1234.com",
+        title: "screencache backend",
+        url: "api.screencache.com",
+        description: "",
+        type: "api"
+    },
+    {
+        thumbnail: "3456.com",
+        title: "infinesse",
+        url: "infinesse.com",
+        description: "",
+        type: "website"
+    },
+    {
+        thumbnail: null,
+        title: "samples",
+        url: "api.infinessesound.com",
+        description: "",
+        type: "website"
+    }
+];
+
 //forEach to add property to the sites array site objects, but logs undefined because forEach does not have a return.. so second log to check that it worked
 console.log(sites.forEach(site => {
     site.active = true;
@@ -39,5 +63,28 @@ console.log(sites.filter(site => {
 }));
 //target site within sites array, that has title property with infinessesound.com value, and change active property to false
 
+console.log(sites.toString(sites.site));
 
+
+
+
+
+
+//useful must know array methods
+// toString();
+// join();
+// concat();
+// splice();
+// indexOf();
+// lastIndexOf();
+// forEach();
+// map();
+// filter();
+// reduce();
+// some();
+// every();
+// flat();
+// find();
+// findIndex();
+// sort();
 
